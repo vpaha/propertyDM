2895259205702759b892757ee788df124e6606ac
Andreev, Pavel (Cognizant) <vpaha0@gmail.com>
2026-05-04 21:03:30 -0400

    f

--- Reparo.Client/Program.cs
+++ Reparo.Client/Program.cs
@@ -6,9 +6,12 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWH5cd3RQRmhZVUJ/XEtWYEA=");
-
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        var syncfusionKey = builder.Configuration["Syncfusion:LicenseKey"];
+
+        if (!string.IsNullOrWhiteSpace(syncfusionKey))
+            SyncfusionLicenseProvider.RegisterLicense(syncfusionKey);
 
         builder.Services.AddSyncfusionBlazor(options => { options.Animation = GlobalAnimationMode.Enable; options.EnableRippleEffect = true; });
 
